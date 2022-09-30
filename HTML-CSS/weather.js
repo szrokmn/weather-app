811eaeec886e8d70b5276a36381841ed0bd67fc6
Sezer Ökmen <szrokmn34@gmail.com>
2022-09-30 10:04:51 +0300

    js continue

---
+++ HTML-CSS/weather.js
@@ -0,0 +1,4 @@
+const form = document.querySelector("section.top-banner form"); 
+const input = document.querySelector(".container input");
+const msg = document.querySelector("span.msg");
+const list = document.querySelector(".ajax-section ul.cities");      
